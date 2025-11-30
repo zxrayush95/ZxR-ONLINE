49dc39f5608066d5667559c9c955ca159b5e1b4b
zxrayush95 <tyren35678@gmail.com>
2025-12-01 03:27:17 +0530

    Update HxCBeta.js

--- HxCBeta.js
+++ HxCBeta.js
@@ -1,4 +1,4 @@
 {
   "version": "0.3",
-  "libLink": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libHxCBeta.so"
+  "url": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libHxCBeta.so"
 }
