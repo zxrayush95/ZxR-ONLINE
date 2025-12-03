19d79f3405cc4a99eeefe6234e3a32ad116e7c90
zxrayush95 <tyren35678@gmail.com>
2025-12-03 19:22:43 +0530

    Update BGMIX.js v0.97

--- HxCBeta.js
+++ HxCBeta.js
@@ -1,5 +1,5 @@
 {
-  "version": "0.96",
+  "version": "0.97",
   "url": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libHxCBeta.so",
   "libLink": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libHxCBeta.so"
 }
