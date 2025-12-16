2b4dc61935175537df179fc2847093c6bf2427ba
zxrayush95 <tyren35678@gmail.com>
2025-12-16 12:23:26 +0530

    Create DeadLine.js

---
+++ DeadLine.js
@@ -0,0 +1,5 @@
+{
+  "version": "1.65",
+  "url": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libDeadLine.so",
+  "libLink": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libDeadLine.so"
+}
