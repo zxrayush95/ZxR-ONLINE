794a3503953dd7ee4a99f7b48f808a94c8f18ad7
zxrayush95 <tyren35678@gmail.com>
2025-12-19 23:42:15 +0530

    Update BGMIX.js v1.05

--- HxCTEST.js
+++ HxCTEST.js
@@ -1,5 +1,5 @@
 {
-  "version": "1.04",
+  "version": "1.05",
   "url": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libTEST.so",
   "libLink": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libTEST.so"
 }
