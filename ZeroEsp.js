aa68cea1da4a796257b17d8b8c0a012b2c801f81
zxrayush95 <tyren35678@gmail.com>
2025-12-21 04:00:46 +0530

    Update ZeroEsp.js

--- ZeroEsp.js
+++ ZeroEsp.js
@@ -1,4 +1,5 @@
 {
   "version": "1.4",
+  "url": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libZeroEsp.so",  
   "libLink": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libZeroEsp.so"
 }
