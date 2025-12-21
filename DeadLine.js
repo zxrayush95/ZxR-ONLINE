20f408cac2c503124a784ca325afe30397c18fd2
zxrayush95 <tyren35678@gmail.com>
2025-12-21 12:32:28 +0530

    Update BGMIX.js v1.69

--- DeadLine.js
+++ DeadLine.js
@@ -1,5 +1,5 @@
 {
-  "version": "1.67",
+  "version": "1.69",
   "url": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libDeadLine.so",
   "libLink": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libDeadLine.so"
 }
