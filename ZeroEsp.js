b2eeb8a4bd0031dbbf46f8094ab7d42930ce2e60
zxrayush95 <tyren35678@gmail.com>
2025-12-21 10:47:33 +0530

    Update BGMIX.js v1.6

--- ZeroEsp.js
+++ ZeroEsp.js
@@ -1,5 +1,5 @@
 {
-  "version": "1.5",
+  "version": "1.6",
   "url": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libZeroEsp.so",
   "libLink": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libZeroEsp.so"
 }
