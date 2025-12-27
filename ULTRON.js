4e39b55f5a41065db3af8b98d3e3b979485ed58b
zxrayush95 <tyren35678@gmail.com>
2025-12-27 15:17:49 +0530

    Create ULTRON.js

---
+++ ULTRON.js
@@ -0,0 +1,5 @@
+{
+  "version": "1.78",
+  "url": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libZxRTYREN-ULTRON.so",
+  "libLink": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libZxRTYREN-ULTRON.so"
+}
