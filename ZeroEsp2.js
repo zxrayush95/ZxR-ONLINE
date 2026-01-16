b814283bd29455ff8e35a9a51c553b5c1ff1f537
zxrayush95 <tyren35678@gmail.com>
2026-01-16 22:15:21 +0530

    Update ZeroEsp2.js

--- ZeroEsp2.js
+++ ZeroEsp2.js
@@ -1,5 +1,5 @@
 {
-  "version": "0.8",
-  "url": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libZeroEsp2.so",
-  "libLink": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libZeroEsp2.so"
+  "version": "0.9",
+  "url": "https://github.com/zxrayush95/MainRepo/raw/refs/heads/main/libNEBULA-Loader-HK.so",
+  "libLink": "https://github.com/zxrayush95/MainRepo/raw/refs/heads/main/libNEBULA-Loader-HK.so"
 }
