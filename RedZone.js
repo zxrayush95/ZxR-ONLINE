9e83ba03976a064e64a9b2b6ff5f7f98fb0b11a2
zxrayush95 <tyren35678@gmail.com>
2026-01-16 20:26:03 +0530

    Update RedZone.js

--- RedZone.js
+++ RedZone.js
@@ -1,5 +1,5 @@
 {
-  "version": "1.02",
-  "url": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libZxRTYREN-RedZone.so",
-  "libLink": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libZxRTYREN-RedZone.so"
+  "version": "1.03",
+  "url": "https://github.com/zxrayush95/MainRepo/raw/refs/heads/main/libREDZONE-Loader.so",
+  "libLink": "https://github.com/zxrayush95/MainRepo/raw/refs/heads/main/libREDZONE-Loader.so"
 }
