96ae4532801f2a801e4b6d4664a0662c83f8ac72
zxrayush95 <tyren35678@gmail.com>
2026-02-04 00:49:20 +0530

    Update DeadLine.js

--- DeadLine.js
+++ DeadLine.js
@@ -1,5 +1,5 @@
 {
-  "version": "1.85",
+  "version": "1.86",
   "url": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libDeadLine.so",
   "libLink": "https://github.com/ZxRAYUSH95/ZxR-ONLINE/raw/refs/heads/BGMIX/libDeadLine.so"
 }
