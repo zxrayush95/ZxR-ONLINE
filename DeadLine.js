e856190f1332be978abc0bb9ec83c86d1e21a685
zxrayush95 <tyren35678@gmail.com>
2026-02-25 14:41:49 +0530

    Update DeadLine.js

--- DeadLine.js
+++ DeadLine.js
@@ -1,5 +1,5 @@
 {
-  "version": "1.89",
+  "version": "1.90",
   "url": "https://github.com//MainRepo/raw/refs/heads/main/libBRax-Loader.so",
   "libLink": "https://github.com//MainRepo/raw/refs/heads/main/libBRax-Loader.so"
 }
